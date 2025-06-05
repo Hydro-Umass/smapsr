b833a24b6fb49b8aa395b7b90be109d0923e9205
Kostas Andreadis <kandread@umass.edu>
2025-06-05 16:04:48 -0400

    Replaced MLP with convolutional model

--- smapsr/models.py
+++ smapsr/models.py
@@ -1,24 +1,59 @@
+import jax
 import jax.nn as jnn
+import jax.numpy as jnp
+import jax.random as jr
 import equinox as eqx
 import diffrax
+from typing import List, Tuple
 
-class Func(eqx.Module):
-    mlp: eqx.nn.MLP
+class ConvFunc(eqx.Module):
+    layers: List[eqx.Module]
+    activation: callable
+    height: int
+    width: int
+    mask: jax.Array
 
-    def __init__(self, data_size, width_size, depth, *, key, **kwargs):
+    def __init__(self, mask: jax.Array, channels: List[int], kernel_size: int, stride: int = 1, *, key, **kwargs):
         super().__init__(**kwargs)
-        self.mlp = eqx.nn.MLP(in_size=data_size, out_size=data_size, width_size=width_size,
-                              depth=depth, activation=jnn.tanh, key=key)
+        keys = jr.split(key, len(channels) - 1)
+        self.height, self.width = mask.shape
+        self.mask = mask
+        self.layers = []
+        for i in range(len(channels) - 1):
+            conv_layer = eqx.nn.Conv2d(
+                channels[i], channels[i + 1],
+                kernel_size=kernel_size,
+                stride=stride,
+                padding='SAME',
+                key=keys[i]
+            )
+            norm_layer = eqx.nn.GroupNorm(groups=1, channels=channels[i + 1])
+            combined_layer = eqx.nn.Sequential([conv_layer, norm_layer])
+            # self.layers.append(combined_layer)
+            self.layers.append(conv_layer)
+        self.activation = jnn.swish
 
-    def __call__(self, t, y, args):
-        return self.mlp(y)
+    def __call__(self, t, yv, args):
+        # un-flatten the image data
+        y = yv.reshape(yv.shape[0], self.height, self.width)
+        for layer in self.layers:
+            # apply convolution only on valid data
+            # y = layer(y * self.mask)
+            y = layer(y)
+            y = self.activation(y)
+        y = y.reshape(y.shape[0], -1)  # flatten back to vector form
+        return y
 
 class NeuralODE(eqx.Module):
-    func: Func
+    func: ConvFunc
+    mask: jax.Array
+    valid: jax.Array
 
-    def __init__(self, data_size, width_size, depth, *, key, **kwargs):
+    def __init__(self, mask: jax.Array , channels: List[int], kernel_size: int, stride: int = 1, *, key, **kwargs):
         super().__init__(**kwargs)
-        self.func = Func(data_size, width_size, depth, key=key)
+        self.mask = mask
+        self.valid = mask.reshape(1, -1).nonzero()[1]
+        self.func = ConvFunc(mask=mask, channels=channels, kernel_size=kernel_size, stride=stride,  key=key)
 
     def __call__(self, ts, y0):
         solution = diffrax.diffeqsolve(
@@ -28,7 +63,6 @@ class NeuralODE(eqx.Module):
             t1=ts[-1],
             dt0=ts[1] - ts[0],
             y0=y0,
-            #stepsize_controller=diffrax.PIDController(rtol=1e-3, atol=1e-6),
             saveat=diffrax.SaveAt(ts=[ts[-1]]),
         )
         return solution.ys
